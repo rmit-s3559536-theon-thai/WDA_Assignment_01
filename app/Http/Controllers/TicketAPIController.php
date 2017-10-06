6509393295b0572687eb07f070a7735cd3ab4201
allencho <s3558745@student.rmit.edu.au>
2017-10-06 15:55:06 +1100

    API working for adding comments to the database

--- app/Http/Controllers/TicketAPIController.php
+++ app/Http/Controllers/TicketAPIController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\TicketDetails;
 use App\User;
+use App\Comment;
 
 use Illuminate\Routing\ResponseFactory;
 use Illuminate\Http\Request;
@@ -43,22 +44,37 @@ class TicketAPIController extends Controller
 
     public function store(Request $request)
     {
-        $ticket = new TicketDetails;
 
-        $ticket->os = $request->input('os');
-        $ticket->issue = $request->input('issue');
-        $ticket->status = $request->input('status');
-        $ticket->priority = $request['priority'];
-        $ticket->escLevel = $request['escLevel'];
-        $ticket->comments->comment = $request['comment'];
-        $ticket->user_id = $request['user_id'];
 
-        if ($ticket->save()) {
-            return $ticket;
+        $allRequest = $request->all();
+
+        $ticket_details = new TicketDetails();
+        $ticket_details->os = $allRequest['os'];
+        $ticket_details->issue = $allRequest['issue'];
+        $ticket_details->status = $allRequest['status'];
+        $ticket_details->priority = $allRequest['priority'];
+        $ticket_details->escLevel = $allRequest['escLevel'];
+        $ticket_details->user_id = $allRequest['user_id'];
+        $ticket_details->save();
+
+
+        $ticketComments = new Comment();
+        $ticketComments->comment = $allRequest['comment'];
+
+        $ticketComments->ticket_details_id = $ticket_details->id;
+        $ticketComments->save();
+
+
+
+
+
+        if ($ticket_details->save()) {
+            return $ticket_details;
         }
 
         throw new HttpException(400, "Invalid data");
     }
+
 
     public function update(Request $request, $id)
     {
@@ -83,14 +99,14 @@ class TicketAPIController extends Controller
         throw new HttpException(400, "Invalid data");
     }
 
-    public function updateEscPriLvl(Request $request, $id)
+    public function updateEscLevel(Request $request, $id)
     {
         if (!$id) {
             throw new HttpException(400, "Invalid id");
         }
 
         $ticket = TicketDetails::find($id);
-        $ticket->priority = $request['priority'];
+//        $ticket->priority = $request['priority'];
         $ticket->escLevel = $request['escLevel'];
 
         if ($ticket->save()) {
@@ -100,7 +116,53 @@ class TicketAPIController extends Controller
         throw new HttpException(400, "Invalid data");
     }
 
+    public function updatePriority(Request $request, $id)
+    {
+        if (!$id) {
+            throw new HttpException(400, "Invalid id");
+        }
 
+        $ticket = TicketDetails::find($id);
+        $ticket->priority = $request['priority'];
+//        $ticket->escLevel = $request['escLevel'];
+
+        if ($ticket->save()) {
+            return $ticket;
+        }
+
+        throw new HttpException(400, "Invalid data");
+    }
+
+    public function updateStatus(Request $request, $id)
+    {
+        if (!$id) {
+            throw new HttpException(400, "Invalid id");
+        }
+
+        $ticket = TicketDetails::find($id);
+        $ticket->status = $request['status'];
+//        $ticket->escLevel = $request['escLevel'];
+
+        if ($ticket->save()) {
+            return $ticket;
+        }
+
+        throw new HttpException(400, "Invalid data");
+    }
+
+    public function addComment(Request $request)
+    {
+        $comment = new Comment();
+
+        $comment->comment = $request['comment'];
+        $comment->ticket_details_id = $request['ticket_details_id'];
+
+        if ($comment->save()) {
+            return $comment;
+        }
+
+        throw new HttpException(400, "Invalid data");
+    }
 
     public function destroy($id)
     {
